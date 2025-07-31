9141ea7666c0054ab8eaf19a9e4325407a967214
SHARANYA-0529 <sharanyagoud029@gmail.com>
2025-07-31 21:25:29 +0530

    first commit

---
+++ javascript/intro2.js
@@ -0,0 +1,3 @@
+function sayHello(){
+    document.getElementById("output").innerHTML="you clicked the button";
+}
---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").onclick=function(){
+    alert("Button clicked!")
+}
+console.log()
